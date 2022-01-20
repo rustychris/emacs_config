4e81bdb3499eb833fc2dc4ad253e5aa397f94e5e
Rusty Holleman <rusty@rmanet.com>
2022-01-19 16:00:36 -0800

    windows changes, refactor

--- lisp/local.el
+++ lisp/local.el
@@ -1,22 +1,32 @@
-(setq path "/home/rusty/miniconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")
-(setq path "/home/rusty/miniconda3/envs/general/bin:/home/rusty/miniconda3/condabin:home/rusty/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin")
-(setenv "PATH" path)
+(setq python-shell-interpreter "ipython")
 
-(setenv "PROJ_LIB" "/home/rusty/miniconda3/envs/general/share/proj")
+(if (display-graphic-p)
+    (progn
+      (setq python-shell-interpreter-args "--simple-prompt --matplotlib=qt")
+      ;; laptop, this is about right
+      (set-face-attribute 'default nil :height 140))
+  (setq python-shell-interpreter-args "--simple-prompt --matplotlib=agg"))
 
-; (setenv "PYTHONPATH" "/Users/rusty/python")
-(setq exec-path (append exec-path '("/home/rusty/miniconda3/envs/general/bin")))
-(setq exec-path (append exec-path '("/usr/local/bin")))
-
-; (add-to-list 'load-path "/Users/rusty/.emacs.d/elpa/python-cell-20131029.1616")
+  
+(if (string-match "linux" system-configuration)
+    (progn
+      (setq path "/home/rusty/miniconda3/envs/general/bin:/home/rusty/miniconda3/condabin:/home/rusty/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin")
+      (setenv "PATH" path)
+      (setenv "PROJ_LIB" "/home/rusty/miniconda3/envs/general/share/proj")
+      (setq exec-path (append exec-path '("/home/rusty/miniconda3/envs/general/bin")))
+      (setq exec-path (append exec-path '("/usr/local/bin")))
+      ))
 
 ; python.el settings:
 ; (setq python-shell-interpreter "/home/rusty/anaconda/bin/ipython")
 ; (setq python-shell-interpreter-args "--pylab=tk") ; This had been commented out - why?
 
-; or on linux:
-(setq python-shell-interpreter "ipython")
-(setq python-shell-interpreter-args "--simple-prompt --matplotlib=qt")
+(if (string-match "w64" system-configuration)
+    (setq python-shell-interpreter "C:\\Users\\rusty\\src\\ipython-general.bat"
+          python-shell-interpreter-args "")
+
+;; (setq python-shell-interpreter "C:\\Users\\rusty\\Miniconda3\\envs\\general\\Scripts\\jupyter"
+;;       python-shell-interpreter-args "console --simple-prompt")
 
 (defun run-python-cws ()
   "Run a python shell on cws-linuxmodeling.  Currently this
@@ -25,4 +35,4 @@
   (let ((python-shell-interpreter "/opt/anaconda3/bin/ipython")
         (python-shell-interpreter-args "--simple-prompt --matplotlib=agg"))
     (run-python)))
-     
+
